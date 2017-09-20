6e0f9ae5c4d6ed4a7b8cf5a668e3c31774d75e36
dididy <dididy@knou.ac.kr>
2017-09-20 18:21:31 +0900

    Add files via upload

---
+++ 06.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "stdafx.h"
+
+int main()
+{
+	int p_1 = 95;
+	int p_2 = 84;
+	int total = 0;
+
+	total = p_1 + p_2;
+	printf("%d",total);
+    return 0;
+}
+
---
+++ 07.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int p_1 = 95;
+	int p_2 = 84;
+	int total=0;
+	int avg_1 = 0;
+	float avg_2 = 0;
+	total = p_1 + p_2;
+	printf("ÇÕ:%d\n", total);
+	avg = total/2;
+	avg2 =total/2.0;
+	printf("Á¤¼ö Æò±Õ:%d\n", avg_1);
+	printf("½Ç¼ö Æò±Õ:%f\n", avg_22);
+}
