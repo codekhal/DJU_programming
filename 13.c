c8d1750362ffa1c13e332d8d8a09c3d800c87eca
dididy <dididy@knou.ac.kr>
2017-09-21 19:57:28 +0900

    Rename 13.cpp to 13.c

---
+++ 13.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("\a");	//비프음 발생
+	printf("수업시간입니다.\n");
+}
--- 13.cpp
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(void)
-{
-	printf("\a");	//비프음 발생
-	printf("수업시간입니다.\n");
-}
